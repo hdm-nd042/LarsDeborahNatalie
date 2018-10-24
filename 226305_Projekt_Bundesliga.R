24e64dbccad93b16389e73880067e5f3eb4d9fd0
DanielSebhatu <ds160@hdm-stuttgart.de>
2018-10-24 09:36:55 +0200

    Updated R Script

--- 226305_Projekt_Bundesliga.R
+++ 226305_Projekt_Bundesliga.R
@@ -8,7 +8,7 @@ library("visNetwork")
 elBundesliga<- read.csv("AllinOne - Edgelist.csv",header=T, as.is=T)
 nlBundesliga<- read.csv("AllinOne - Nodelist.csv", header=T, as.is=T)
 
-# Prüfen der Daten
+# PrÃ¼fen der Daten
 head(elBundesliga)
 head(nlBundesliga)
 
@@ -31,14 +31,14 @@ E(bundesliga)$color <- "lightblue"
 E(bundesliga)$arrow.size <- .4
 plot(bundesliga)
 
-# Netzwerkmaße berechnen
+# NetzwerkmaÃŸe berechnen
 is_connected(bundesliga)
 components(bundesliga)
 edge_density(bundesliga)
 triad_census(bundesliga)
 distances(bundesliga, v = V(bundesliga), to = V(bundesliga))
 
-# Akteurmaße berechnen
+# AkteurmaÃŸe berechnen
 degree(bundesliga)
 degree(bundesliga, V(bundesliga)$type == "2", mode = c("in"), loops = TRUE, normalized = TRUE)
 eigen_centrality(bundesliga)
@@ -70,7 +70,7 @@ visIgraph(bundesliga2011, type = "full")
 
 # Egonetzwerk Jerome Boateng
 
-# Frage: Beim Ausführen des Plot-Befehls erscheint das Ego-Netzwerk von Boateng, jedoch mit falschen Edge Labels. Woran liegt das ?
+# Frage: Beim AusfÃ¼hren des Plot-Befehls erscheint das Ego-Netzwerk von Boateng, jedoch mit falschen Edge Labels. Woran liegt das ?
 # edge.label=Boateng[[1]]$time || Boateng, da wir seine edges wollen und nicht die der Bundesliga
 Boateng <- make_ego_graph(bundesliga, order=1, c("Jerome Agyenim Boateng"))
 plot(Boateng[[1]],  edge.arrow.size=0.1, vertex.color="green", vertex.size=2, edge.color = "lightgrey", edge.label=Boateng[[1]]$time,edge.label.cex=c(1), vertex.label.color="black", vertex.frame.color="transparent")
@@ -84,7 +84,7 @@ nlBayern<- read.csv("FC Bayern Muenchen - Nodelist.csv", header=T, as.is=T)
 elVfB<- read.csv("VfB Stuttgart - Edgelist.csv",header=T, as.is=T)
 nlVfB<- read.csv("VfB Stuttgart - Nodelist.csv", header=T, as.is=T)
 
-# Prüfen der Daten
+# PrÃ¼fen der Daten
 head(elBayern)
 head(nlBayern)
 
@@ -143,7 +143,7 @@ nlMueller<- read.csv("Mueller - Nodelist.csv", header=T, as.is=T)
 elGomez<- read.csv("Gomez - Edgelist.csv",header=T, as.is=T)
 nlGomez<- read.csv("Gomez - Nodelist.csv", header=T, as.is=T)
 
-# Prüfen der Daten
+# PrÃ¼fen der Daten
 head(elMueller)
 head(nlMueller)
 
@@ -161,22 +161,22 @@ gomez <- tbl_graph(
 
 #Arc diagram Mueller
 ggraph(mueller, layout = "linear" ) + 
-  geom_edge_arc(aes(label = time)) + #aes, damit sich darauffolgende argumente auf das label beziehen, bsp. colour
-  geom_node_text(aes(label = id), size = 3, repel = TRUE)+ #Vereinnamen #repel=True damit nicht überlappen
+  geom_edge_arc(aes(label = time)) + 
+  geom_node_text(aes(label = id), size = 3, repel = TRUE)+ 
   theme_graph()
 
 #Arc diagram Gomez
 ggraph(gomez, layout = "linear" ) + 
-  geom_edge_arc(aes(label = time)) + #aes, damit sich darauffolgende argumente auf das label beziehen, bsp. colour
-  geom_node_text(aes(label = id), size = 3, repel = FALSE)+ #Vereinnamen #repel=True damit nicht überlappen
+  geom_edge_arc(aes(label = time)) + 
+  geom_node_text(aes(label = id), size = 3, repel = FALSE)+ 
   theme_graph()
 
 # Arc diagram mit "Beispiel" Hintergrundbild
-ima <- readPNG("C:/Users/ds160/Desktop/226305_Projekt_Bundesliga_final/FC_Bayern_Muenchen.png")
+ima <- readPNG("https://raw.githubusercontent.com/hdm-nd042/LarsDeborahNatalieDaniel/master/FC_Bayern_Muenchen.png")
 ggraph(mueller, layout = "linear") + 
   annotation_custom(rasterGrob(ima, width = unit(0.25,"npc"), height = unit(0.25,"npc")),-Inf, Inf, -Inf, Inf)+
-  geom_edge_arc(aes(label = time)) + #aes, damit sich darauffolgende argumente auf das label beziehen, bsp. colour
-  geom_node_text(aes(label = id), size = 5, repel = TRUE)+ #Vereinnamen #repel=True damit nicht überlappen
+  geom_edge_arc(aes(label = time)) + 
+  geom_node_text(aes(label = id), size = 5, repel = TRUE)+ 
   theme_graph()
 
 
